7f1d82b959120964692518d94ba685ed067f0c75
harshams <hmsridhar@gigsky.com>
2020-04-23 16:43:43 +0530

    day1 UI changes made

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  currentImagePath: string;
+  passcode: string;
+  answer: string;
+  shipEntry = true;
+  cityGate = false;
+  cityMain = false;
+  shipExit = false;
+  
+
+  constructor() { 
+    this.currentImagePath = "url('../../assets/ship.jpg')";  
+  }
 
   ngOnInit(): void {
   }
 
+  enterCity(){
+    this.currentImagePath ="url('../../assets/door1.jpg')";
+    this.shipEntry = false;
+    this.cityGate = true;
+  }
+
+  openGateIfAnswerValid(){
+    if(this.passcode == "sudo"){
+      this.passcode="";
+      this.cityGate = false;
+      this.cityMain = true;
+      this.currentImagePath = "url('../../assets/city1.png')"
+    }
+  }
+
+  getKeyIfAnswerValid(){
+    if(this.answer == "sudo"){
+      this.answer = "";
+      this.cityMain = false;
+      this.shipExit = true;
+      this.currentImagePath="url('../../assets/shipExit.webp')"
+    }
+  }
 }
